0df02ecd11e1739cff2e9e59bb97cbded9384db4
Ben Hurst <BHurst8@gmail.com>
2018-08-21 22:34:55 -0700

    added tests, README

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,4 +1,5 @@
 import requests as req
+from textwrap import dedent
 
 
 def test_server_get_home_route_status_200():
@@ -8,8 +9,25 @@ def test_server_get_home_route_status_200():
 
 def test_server_get_home_route_response_content():
     response = req.get('http://127.0.0.1:5000')
-    assert '<html><body><h1>Hello world</h1></body></html>' == str(response.text)
-    assert 'Hello world' in str(response.text)
+    expected = dedent('''
+        <html>
+        <head>
+            <title> cowsay </title>
+        </head>
+        <body>
+            <header>
+                <nav>
+                <ul>
+                    <li><a href="/cow">cowsay</a></li>
+                </ul>
+                </nav>
+            <header>
+            <main>
+            </main>
+        </body>
+        </html>''')
+
+    assert response.text == expected
 
 
 def test_server_get_returns_404_on_bad_path():
@@ -33,22 +51,16 @@ def test_server_post_cow_route_returns_400_without_query():
 
 
 def test_server_get_cow_returns_a_talking_steggy():
+    '''This function tests that a valid get requests sends back the talking dinosaur. Note: Had some difficulty testing the nicely formatted one since it had some single curly braces in it which threw off the f-string. Normal string didn't capture carriage returns properly.
+    '''
     response = req.get('http://127.0.0.1:5000/cow?msg=hi')
-    expected = ''' ____
-                < hi >
-                ----
-                \                             .       .
-                \                           / `.   .' "
-                \                  .---.  <    ><    >  .---.
-                \                 |    \  \ - ~ ~ - /  /    |
-                        _____          ..-~             ~-..-~
-                        |     |   \~~~\.'                    `./~~~/
-                    ---------   \__/                        \__/
-                    .'  O    \     /               /       \  "
-                    (_____,    `._.'               |         }  \/~~~/
-                    `----.          /       }     |        /    \__/
-                            `-.      |       /      |       /      `. ,~~|
-                                ~-.__|      /_ - ~ ^|      /- _      `..-'
-                                    |     /        |     /     ~-.     `-. _  _  _
-                                    |_____|        |_____|         ~ - . _ _ _ _ _>'''
-    pass
+    expected = b''' ____ \n< hi >\n ---- \n\\                             .       .\n \\                           / `.   .\' " \n  \\                  .---.  <    > <    >  .---.\n   \\                 |    \\  \\ - ~ ~ - /  /    |\n         _____          ..-~             ~-..-~\n        |     |   \\~~~\\.\'                    `./~~~/\n       ---------   \\__/                        \\__/\n      .\'  O    \\     /               /       \\  " \n     (_____,    `._.\'               |         }  \\/~~~/\n      `----.          /       }     |        /    \\__/\n            `-.      |       /      |       /      `. ,~~|\n                ~-.__|      /_ - ~ ^|      /- _      `..-\'   \n                     |     /        |     /     ~-.     `-. _  _  _\n                     |_____|        |_____|         ~ - . _ _ _ _ _>'''
+    assert response.text.encode() == expected
+
+
+def test_server_post_cow_returns_a_json_talking_steggy():
+    '''This function tests that a valid post requests sends back a json object with a 'content' key and the byte-encoded cowpy image
+    '''
+    response = req.post('http://127.0.0.1:5000/cow?msg=hi')
+    expected = {"content": " ____ \n< hi >\n ---- \n\\                             .       .\n \\                           / `.   .' \" \n  \\                  .---.  <    > <    >  .---.\n   \\                 |    \\  \\ - ~ ~ - /  /    |\n         _____          ..-~             ~-..-~\n        |     |   \\~~~\\.'                    `./~~~/\n       ---------   \\__/                        \\__/\n      .'  O    \\     /               /       \\  \" \n     (_____,    `._.'               |         }  \\/~~~/\n      `----.          /       }     |        /    \\__/\n            `-.      |       /      |       /      `. ,~~|\n                ~-.__|      /_ - ~ ^|      /- _      `..-'   \n                     |     /        |     /     ~-.     `-. _  _  _\n                     |_____|        |_____|         ~ - . _ _ _ _ _>"}
+    assert response.json() == expected
